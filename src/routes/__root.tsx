e25d374836b3af107051dbce2cac8fde0446fb37
Barry Michael Doyle <barry@barrymichaeldoyle.com>
2026-02-06 23:18:19 +0200

    fixed favicon for apple devices

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -92,7 +92,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
     links: [
       { rel: 'stylesheet', href: appCss },
       { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-      { rel: 'apple-touch-icon', href: '/logo192.png' },
+      { rel: 'apple-touch-icon', href: '/favicon.svg' },
       { rel: 'manifest', href: '/manifest.json' },
       { rel: 'canonical', href: siteConfig.url },
     ],
